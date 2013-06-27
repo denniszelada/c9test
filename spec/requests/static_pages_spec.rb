c1a14b41a451d4236025d165e6d1899a71ab28d5
Dennis Zelada <denniszelada@gmail.com>
2013-06-27 13:53:47 -0400

    changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-<<<<<<< HEAD
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
-=======
+    
   describe "Home Page" do
+      
       it "should have the h1 'Sample App'" do
           visit '/static_pages/home'
           page.should have_selector('h1', :text => 'Sample App')
       end
       
-      it "should have the title 'Home'" do
+      it "should have the base title" do
           visit '/static_pages/home'
-          page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+          page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+      end
+      
+      it "should not have a custom page title" do
+          visit '/static_pages/home'
+          page.should_not have_selector('title', :text => '| Home')
+
       end
   end
   
@@ -50,5 +49,5 @@ describe "StaticPages" do
     end
   end
   
->>>>>>> feature/static-pages
+
 end
